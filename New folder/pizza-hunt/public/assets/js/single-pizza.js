98e9cef857cfc9ae70161937883d24e7636dd62a
cody-junier <codyjunier@gmail.com>
2022-02-09 19:38:38 -0800

    Stuck on reply

--- New folder/pizza-hunt/public/assets/js/single-pizza.js
+++ New folder/pizza-hunt/public/assets/js/single-pizza.js
@@ -9,6 +9,26 @@ const $newCommentForm = document.querySelector('#new-comment-form');
 
 let pizzaId;
 
+function getPizza(){
+  const searchParams = new URLSearchParams(document.location.search.substring(1));
+  const pizzaId = searchParams.get('id')
+
+  fetch(`/api/pizzas/${pizzaId}`)
+    .then(response => {
+      if(!response.ok){
+        throw new Error ({ message: 'Something went wrong!'})
+      }
+      console.log(response)
+      return response.json()
+    })
+    .then(printPizza)
+    .catch(err=>{
+      console.log(err)
+      alert("Can't find this pizza, throwing you back")
+      window.history.back();
+    })
+}
+
 function printPizza(pizzaData) {
   console.log(pizzaData);
 
@@ -87,6 +107,28 @@ function handleNewCommentSubmit(event) {
   }
 
   const formData = { commentBody, writtenBy };
+  fetch(`/api/comments/${pizzaId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if(!response.ok){
+      throw new Error ({ message: 'Something went wrong!'})
+    }
+    console.log(response)
+    response.json()
+  })
+  .then(commentResponse => {
+    console.log(commentResponse)
+    location.reload()
+  })
+  .catch(err=>{
+    console.log(err)
+  })
 }
 
 function handleNewReplySubmit(event) {
@@ -114,3 +156,4 @@ $backBtn.addEventListener('click', function() {
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
+getPizza();
